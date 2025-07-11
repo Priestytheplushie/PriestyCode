f2052ed84db43e1f60107d7751aa5a8eb3eb81dc
Priesty <142067979+Priestytheplushie@users.noreply.github.com>
2025-07-10 23:34:46 -0400

    Test Commit 12

---
+++ src/test.py
@@ -0,0 +1,6 @@
+# Create a new file: src/temp_test.py
+import sys, os # Flake8 Error: multiple imports on one line
+
+def bad_function () : # Black Error: bad formatting
+    unused_variable = "hello" # Flake8 Error: unused variable
+    return True
--- src/test2.py
+++
@@ -1,9 +0,0 @@
-import heapq
-import turtle
-
-try:
-    print("hello")
-except ValueError:
-    print(no)
-
-print(turtle)
