5a0d4dd857e6661904e6ee318963b63150551257
Priesty <142067979+Priestytheplushie@users.noreply.github.com>
2025-07-11 03:27:32 -0400

    Fixed Test.py

--- src/test.py
+++ src/test.py
@@ -1,8 +1,15 @@
 import py_compile
+import tkinter
+import time
+import pickle
 
-def dumb_function(pass):
-    unused_var = 10
-    used_var = 200
-    return "screw you"
+var = 20
+
+def goofy_function(goof):
+    goof2 = 1
+    lol = "lol"
+
+def another_function():
+    hello = 10
 
 print(py_compile)
